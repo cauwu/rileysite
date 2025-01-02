c4e940776f8b39f3b5923294d030fb1f653087be
cauwu <6922384+cauwu@users.noreply.github.com>
2025-01-02 02:52:26 +0000

    mobile support

--- rileysite/static/js/modules/rileysite.js
+++ rileysite/static/js/modules/rileysite.js
@@ -1,4 +1,24 @@
 window.addEventListener("DOMContentLoaded", () => {
-	
-})
+	emailcopysystem();
+});
 
+const emailcopysystem = () => {
+    document.querySelectorAll('#titlecontact').forEach((button) => {
+        button.addEventListener('mousedown', () => {
+            navigator.clipboard.writeText('riley@' + 'riley.com');
+            showhidecopiednotif();
+        });
+    });
+}
+
+let timeout;
+const showhidecopiednotif = () => {
+    const notif = document.getElementById('emailcopiednotif');
+    notif.innerHTML = "✉️ Copied!";
+    clearTimeout(timeout);
+    timeout = setTimeout(hidenotif, 3000);
+}
+
+const hidenotif = () => {
+    document.getElementById('emailcopiednotif').innerHTML = "";
+}
